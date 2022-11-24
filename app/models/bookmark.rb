983626aa071c180eb9715a79790c02b0718450aa
Olga Grigoryeva <33clouds@gmail.com>
2022-11-24 20:04:30 +0100

    Back end done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,6 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6 }
-  validates :list, uniqueness: { scope: :movie}
+  validates :list, uniqueness: { scope: :movie,
+  message: "Only one reservation per guest per day is permitted"}
 end
